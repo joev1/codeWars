d65937967472d3981c26b3a95f17b56ae600815f
JoeV <joe.veror@gmail.com>
2019-05-19 15:36:16 +0600

    8kyu

---
+++ PHP/8kyu.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+ * */
+
+function sum_mix($a) {
+    return array_sum($a);
+}
+
+/*
+ * Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot seperating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+Patrick Feeney => P.F
+
+ * */
+function abbrevName($name){
+    return;
+}
